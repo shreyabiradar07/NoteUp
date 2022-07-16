531cf3a439cc3677535c0f0f1cfe1aa896a8efd9
Shreya Biradar <biradarshreya24@gmail.com>
2022-07-16 23:51:14 +0530

    init project

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const noteRoutes = require('./routes/noteRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  })
+);
+dotenv.config();
+
+connectDB();
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/notes', noteRoutes);
+// ----------Deployment----------
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running..');
+  });
+}
+
+// ----------Deployment----------
+
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
